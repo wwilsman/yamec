764ad2bc3cc17cafdc99f3fc0e9e6395da93e069
Wil Wilsman <wil.wilsman@ymail.com>
2015-01-27 16:52:49 -0500

    Add multi-toolbar support

--- src/js/editor.js
+++ src/js/editor.js
@@ -18,6 +18,7 @@ class Editor {
     this.dom.setAttribute('contentEditable', true);
 
     this.selection = new Selection();
+    this.toolbars = {};
     this.commands = {};
     this.events = {};
 
@@ -95,15 +96,14 @@ class Editor {
         document.execCommand(cmd, false, value);
       }
 
-      // collapse selection and hide toolbar
+      // collapse selection
       if (collapse) {
         this.selection.collapseToEnd();
-        this.toolbar.hide();
-
-      // reposition the toolbar
-      } else {
-        this.showToolbar();
       }
+
+      // toggle toolbar
+      // TODO: Remove the toolbar from this logic
+      this.trigger('toolbar:inline');
     });
   }
 
@@ -112,6 +112,12 @@ class Editor {
 
     // split names into an array
     names = names.split(' ');
+
+    // if `func` is string, wrap in trigger
+    if (typeof func === 'string') {
+      let triggerName = func;
+      func = () => this.trigger(triggerName);
+    }
 
     // add events
     for (let name of names) {
@@ -153,11 +159,19 @@ class Editor {
 
   // trigger events attached to the editor
   trigger(name) {
-    var funcs;
+    var m, funcs,
+      args = _.slice(arguments, 1);
 
+    // pass toolbar for toolbar events
+    if (m = /^toolbar:(.*)$/.exec(name)) {
+      args.unshift(this.toolbars[m[1]]);
+    }
+
+    // call all the functions if any exist
     if (funcs = this.events[name]) {
       for (let func of funcs) {
-        func.apply(this, _.slice(arguments, 1));
+        // defer for some events
+        _.defer(() => func.apply(this, args));
       }
     }
 
@@ -189,9 +203,32 @@ class Editor {
     // default events
 
     // show toolbar when a selection might be made
-    this.on('mouseup keyup', this.showToolbar);
+    this.on('mouseup keyup', 'toolbar:inline');
 
     return this;
+  }
+
+  // stores a toolbar by name and adds toggle event
+  addToolbar(name, toolbar, toggle) {
+    this.toolbars[name] = toolbar;
+    this.on(`toolbar:${name}`, toggle);
+  }
+
+  // removes a toolbar and all it's attached events
+  detachToolbar(name) {
+    var toolbar = this.toolbars[name];
+
+    if (toolbar) {
+      delete this.toolbars[name];
+      this.off(`toolbar:${name}`);
+    }
+
+    return toolbar;
+  }
+
+  // use method instead of accessing the property directly
+  getToolbar(name) {
+    return this.toolbars[name];
   }
 
   // registers some default commands and initializes the toolbar
@@ -215,12 +252,13 @@ class Editor {
 
         // show input to create link
         } else {
-          this.toolbar.toggleInput('http://...', (value) => {
+          this.getToolbar('inline').toggleInput('http://...', (value) => {
             this.execCommand('createLink', value, !!value);
           });
         }
       },
 
+      // toggle block elements
       'toggleBlock': function (tagName) {
 
         // format as paragraph
@@ -233,6 +271,7 @@ class Editor {
         }
       },
 
+      // toggle tags (used for inline elements)
       'toggleTag': function (tagName) {
         var els;
 
@@ -288,7 +327,7 @@ class Editor {
       };
     };
     
-    this.toolbar = new EditorToolbar({
+    var toolbar = new EditorToolbar({
       onInputCancel: () => {
         this.selection.restore();
       },
@@ -324,57 +363,56 @@ class Editor {
       }]
     });
 
+    // toolbar toggle logic
+    this.addToolbar('inline', toolbar, (t) => {
+      var boundary, toolbarHeight, toolbarHalfWidth,
+        point = {};
+
+      // save selection in case focus changes later
+      this.selection.save();
+      
+      // hide the toolbar if nothing is selected
+      if (this.selection.isCollapsed) {
+        t.hide();
+        return;
+      }
+
+      // get starting points for toolbar
+      boundary = this.selection.getRangeBoundary();
+      point.x = (boundary.left + boundary.right) / 2;
+      point.y = window.pageYOffset;
+
+      // get dimensions of toolbar
+      t.dom.style.display = 'block';
+      toolbarHeight = t.dom.offsetHeight;
+      toolbarHalfWidth = t.dom.offsetWidth / 2;
+      t.dom.style.display = '';
+
+      // center toolbar under selection
+      if (boundary.top < toolbarHeight) {
+        t.position('bottom');
+        point.y += boundary.bottom;
+
+      // center toolbar above selection
+      } else {
+        t.position('top');
+        point.y += boundary.top;
+      }
+
+      // snap toolbar to left edge
+      if (point.x < toolbarHalfWidth) {
+        point.x += toolbarHalfWidth - point.x;
+
+      // snap toolbar to right edge (adjusted for scrollbar)
+      } else if (window.innerWidth - _.scrollbarWidth() - point.x < toolbarHalfWidth) {
+        point.x -= toolbarHalfWidth - (window.innerWidth - point.x - _.scrollbarWidth());
+      }
+
+      // show toolbar
+      t.show(point.x, point.y);
+    });
+
     return this;
-  }
-
-  // shows the toolbar (hides if no selection)
-  // TODO: add context argument to show/hide different toolbars
-  showToolbar() {
-    var boundary, toolbarHeight, toolbarHalfWidth,
-      point = {};
-
-    // save selection in case focus changes later
-    this.selection.save();
-    
-    // hide the toolbar if nothing is selected
-    if (this.selection.isCollapsed) {
-      this.toolbar.hide();
-      return;
-    }
-
-    // get starting points for toolbar
-    boundary = this.selection.getRangeBoundary();
-    point.x = (boundary.left + boundary.right) / 2;
-    point.y = window.pageYOffset;
-
-    // get dimensions of toolbar
-    this.toolbar.dom.style.display = 'block';
-    toolbarHeight = this.toolbar.dom.offsetHeight;
-    toolbarHalfWidth = this.toolbar.dom.offsetWidth / 2;
-    this.toolbar.dom.style.display = '';
-
-    // center toolbar under selection
-    if (boundary.top < toolbarHeight) {
-      this.toolbar.position('bottom');
-      point.y += boundary.bottom;
-
-    // center toolbar above selection
-    } else {
-      this.toolbar.position('top');
-      point.y += boundary.top;
-    }
-
-    // snap toolbar to left edge
-    if (point.x < toolbarHalfWidth) {
-      point.x += toolbarHalfWidth - point.x;
-
-    // snap toolbar to right edge (adjusted for scrollbar)
-    } else if (window.innerWidth - _.scrollbarWidth() - point.x < toolbarHalfWidth) {
-      point.x -= toolbarHalfWidth - (window.innerWidth - point.x - _.scrollbarWidth());
-    }
-
-    // show toolbar
-    this.toolbar.show(point.x, point.y);
   }
 }
 
